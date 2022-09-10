7897d955e184e9a69b46290000a9cac2669f298c
Gustavo Bachur <gustavo.bachur@outlook.com>
2022-09-10 14:33:51 -0300

    Codigo Comentado e validado na AWS

--- main.tf
+++ main.tf
@@ -4,18 +4,27 @@ provider "aws" {
 
 }
 
+#Criação da Instancia EC2
 resource "aws_instance" "dev" {
+  #Looping para criar mais de uma instancia com o mesmo bloco#
    count = 3
+   #Identificador de Imagem - pegamos isso na Propria AWS
    ami = "ami-052efd3df9dad4825"
+   #Tipo de Instancia que sera criada - Tier VM
    instance_type = "t2.micro"
+   #Chave de acesso para conexão no equipamento
    key_name = "terraformaws1"
+   #Tag para Identificação do equipamento na AWS
    tags = {
     Name ="dev${count.index}"
    }
+   #Security Group - Grupo VPC
+   vpc_security_group_ids = ["sg-093e26b2601f05e8c"]
 }
 
+#Criação do Security Group Utilizado pelas EC2
 resource "aws_security_group" "acesso_ssh" {
-  name = "acesso-ssh"
+  name = "acesso-ssh-2"
   description = "Permitir acesso SSH nas instancias EC2 - Security Group"
 
   ingress {
